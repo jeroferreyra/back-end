6319f63aef1f129bc7ca4b0d43c200b94574ed66
jeroferreyra <jeroferre00@gmail.com>
2023-03-06 12:18:42 -0300

    dockerfile final

--- src/main/java/com/portfolio/jeronimo/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/jeronimo/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins= "https://frontendjer.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
