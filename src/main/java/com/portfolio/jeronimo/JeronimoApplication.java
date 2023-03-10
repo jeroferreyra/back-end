5fcd709fce995bf647d34a7fb7f45db3c1e6e9e9
jeroferreyra <jeroferre00@gmail.com>
2023-03-10 11:30:34 -0300

    finalizacion

--- src/main/java/com/portfolio/jeronimo/JeronimoApplication.java
+++ src/main/java/com/portfolio/jeronimo/JeronimoApplication.java
@@ -18,7 +18,7 @@ public class JeronimoApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://frontend-b31c2.web.app/"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://frontendferre.web.app/"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
